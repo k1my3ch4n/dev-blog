18be8c43761596e109b8c6c0767f8930703d1078
yechan <kevin2695@naver.com>
2024-09-03 18:31:22 +0900

    [0903] header 위치 조정

--- packages/blog/src/App.tsx
+++ packages/blog/src/App.tsx
@@ -4,10 +4,14 @@ import MainLogo from './assets/images/main_logo.svg?react';
 const App = () => {
   return (
     <div className={styles.wrapper}>
+      {/* todo: header 분리 예정 */}
       <div className={styles.header}>
-        <MainLogo />
-        <div>Dev Blog</div>
-        <div>Coding Test</div>
+        <div className={styles.logo}>
+          <MainLogo />
+        </div>
+        <div className={styles.item}>Dev Blog</div>
+        <div className={styles.item}>Coding Test</div>
+        <div className={styles.item}>Portfolio</div>
       </div>
     </div>
   );
