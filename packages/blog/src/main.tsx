64c90d4ec686df0ca6eccd8ae65f8e219e917693
yechan <kevin2695@naver.com>
2024-09-05 18:32:02 +0900

    [0905] browser 연결

--- packages/blog/src/main.tsx
+++ packages/blog/src/main.tsx
@@ -3,8 +3,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const enableMocking = async () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mock/browser');
+
+  return worker.start();
+};
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
