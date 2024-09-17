f63cc01ed5cd84e39b99a0762d8f37a63407ef0e
yechan <kevin2695@naver.com>
2024-09-17 19:16:41 +0900

    [0917] introduce 설정

--- packages/blog/src/pages/Main/Main.tsx
+++ packages/blog/src/pages/Main/Main.tsx
@@ -6,15 +6,18 @@ const Main = () => {
       {/* todo : 이미지 수정 예정 */}
       <div className={styles.mainImage}></div>
       <div className={styles.introduce}>
-        <div className={styles.lhs}>
+        <div className={`${styles.item} ${styles.lhs}`}>
           <p>질문하는 FE 개발자 입니다 .</p>
           <p>질문하고 경험한 것들을 기록합니다 .</p>
+          <p>가나다라마바사가나다라마바사</p>
+          <p>가나다라마바사가나다라마바사</p>
         </div>
-        <div className={styles.rhs}>
+        <div className={`${styles.item} ${styles.rhs}`}>
           <p>K1MY3CH4N</p>
           <p>김예찬</p>
         </div>
       </div>
+      <div className={styles.introduceMore}>저에 대해 더 자세하게 알고 싶으신가요 ? 클릭 !</div>
       <div className={styles.projects}>프로젝트</div>
       <div>etc</div>
     </div>
