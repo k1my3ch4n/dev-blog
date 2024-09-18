2f9fa9156d49ec91c29c0b7e4bdc6bbf55afd302
yechan <kevin2695@naver.com>
2024-09-18 23:05:04 +0900

    [0918] 프로젝트 파트

--- packages/blog/src/pages/Main/Main.tsx
+++ packages/blog/src/pages/Main/Main.tsx
@@ -7,8 +7,8 @@ const Main = () => {
       <div className={styles.mainImage}></div>
       <div className={styles.introduce}>
         <div className={`${styles.item} ${styles.lhs}`}>
-          <p>질문하는 FE 개발자 입니다 .</p>
-          <p>질문하고 경험한 것들을 기록합니다 .</p>
+          <p>질문하는 프론트엔드 개발자 입니다 .</p>
+          <p>질문하고 경험한 것들을 기록하고 공유합니다 .</p>
           <p>가나다라마바사가나다라마바사</p>
           <p>가나다라마바사가나다라마바사</p>
         </div>
@@ -17,8 +17,13 @@ const Main = () => {
           <p>김예찬</p>
         </div>
       </div>
-      <div className={styles.introduceMore}>저에 대해 더 자세하게 알고 싶으신가요 ? 클릭 !</div>
-      <div className={styles.projects}>프로젝트</div>
+      <div className={styles.introduceMore}>
+        저에 대해 더 자세하게 알고 싶으신가요 ? 클릭 ! {'>'}
+      </div>
+      <div className={styles.projects}>
+        {/* 좌측 스크롤 */}
+        <div>card</div>
+      </div>
       <div>etc</div>
     </div>
   );
