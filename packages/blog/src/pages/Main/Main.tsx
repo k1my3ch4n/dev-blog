76008828538be784f4c47b0a5f1234fb30321f37
yechan <kevin2695@naver.com>
2024-09-23 16:17:22 +0900

    [0923] introduce 수정

--- packages/blog/src/pages/Main/Main.tsx
+++ packages/blog/src/pages/Main/Main.tsx
@@ -11,8 +11,7 @@ const Main = () => {
         <div className={`${styles.item} ${styles.lhs}`}>
           <p>질문하는 프론트엔드 개발자 입니다 .</p>
           <p>질문하고 경험한 것들을 기록하고 공유합니다 .</p>
-          <p>가나다라마바사가나다라마바사</p>
-          <p>가나다라마바사가나다라마바사</p>
+          <p>( 해당 블로그는 현재 개발 진행중입니다. )</p>
         </div>
         <div className={`${styles.item} ${styles.rhs}`}>
           <p>K1MY3CH4N</p>
