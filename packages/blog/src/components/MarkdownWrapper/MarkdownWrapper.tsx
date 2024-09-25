2054e093864faad1ecbaf892ec5f976860b3e723
yechan <kevin2695@naver.com>
2024-09-25 22:51:08 +0900

    [0925] img, a, p ,h2 설정 추가

--- packages/blog/src/components/MarkdownWrapper/MarkdownWrapper.tsx
+++ packages/blog/src/components/MarkdownWrapper/MarkdownWrapper.tsx
@@ -1,23 +1,39 @@
 import styles from './MarkdownWrapper.module.scss';
 import MarkdownToJSX from 'markdown-to-jsx';
 
+/*
+  todo : 작업해야할 태그
+
+  - h2
+  - pre ( 긴 코드 )
+  - code ( 짧은 코드 )
+  - ul 
+  - li 
+  - ol
+  - p
+  - h3
+  - img
+*/
+
 const MarkdownWrapper = ({ markdown }: { markdown: string }) => {
-  const H2Component = ({ children }) => {
-    return <div className={styles.h2}>{children}</div>;
-  };
+  // const H2Component = ({ children }) => {
+  //   return <h2 className={styles.h2}>{children}</h2>;
+  // };
 
   return (
-    <MarkdownToJSX
-      options={{
-        // wrapper: 'article',
-        // forceBlock: true,
-        overrides: {
-          h2: H2Component,
-        },
-      }}
-    >
-      {markdown}
-    </MarkdownToJSX>
+    <div className={styles.wrapper}>
+      <MarkdownToJSX
+        options={
+          {
+            // overrides: {
+            //   h2: H2Component,
+            // },
+          }
+        }
+      >
+        {markdown}
+      </MarkdownToJSX>
+    </div>
   );
 };
 
