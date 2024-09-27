6ed3eca3bbb04f48b3f81e36f05287b541dd98cd
yechan <kevin2695@naver.com>
2024-09-27 17:11:42 +0900

    [0927] blog detail 상단 영역 설정

--- packages/blog/src/pages/BlogDetail/BlogDetail.tsx
+++ packages/blog/src/pages/BlogDetail/BlogDetail.tsx
@@ -1,13 +1,41 @@
+import styles from './BlogDetail.module.scss';
 import useGetMarkdown from '@src/hooks/useGetMarkdown';
 import { useParams } from 'react-router-dom';
 import MarkdownWrapper from '@src/components/MarkdownWrapper';
+import { MAIN_POSTS } from '@src/constants/posts';
+import { format } from 'date-fns';
 
 const BlogDetail = () => {
   const { postId } = useParams();
 
   const { markdown } = useGetMarkdown(postId);
 
-  return <MarkdownWrapper markdown={markdown} />;
+  const postData = MAIN_POSTS.filter(({ id }) => id === postId);
+
+  console.log(postData);
+
+  if (postData.length < 1) {
+    return null;
+  }
+
+  const { tags, title, createDate } = postData[0];
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.tagWrapper}>
+        {tags.map((tag, index) => {
+          return (
+            <p className={styles.tag} key={index}>
+              {tag}
+            </p>
+          );
+        })}
+      </div>
+      <div className={styles.createDate}>{format(createDate, 'yyyy-MM-dd')}</div>
+      <MarkdownWrapper markdown={markdown} />;
+    </div>
+  );
 };
 
 export default BlogDetail;
