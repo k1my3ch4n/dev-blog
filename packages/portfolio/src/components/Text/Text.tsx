d7f72e6a349c515aaf3a379796af9687fdaee171
Kim <kevin2695@naver.com>
2024-11-19 19:01:19 +0900

    feat(text) : 일반 text 설정 추가

--- packages/portfolio/src/components/Text/Text.tsx
+++ packages/portfolio/src/components/Text/Text.tsx
@@ -1,7 +1,11 @@
 import styles from './Text.module.scss';
 
 const Text = ({ text }: { text: string }) => {
-  return <div className={styles.text}>{text}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.text}>{text}</div>
+    </div>
+  );
 };
 
 export default Text;
