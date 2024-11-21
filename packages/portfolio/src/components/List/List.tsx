cc0ae82be57b748f3bec14503cd54eee0c5d4374
Kim <kevin2695@naver.com>
2024-11-21 14:44:23 +0900

    feat(List) : list 컴포넌트에 크기에 따라서 spot 변경되도록 수정

--- packages/portfolio/src/components/List/List.tsx
+++ packages/portfolio/src/components/List/List.tsx
@@ -1,15 +1,15 @@
 import styles from './List.module.scss';
 
-const List = ({
-  // spot = 'l',
-  children,
-}: {
-  // spot?: 'l' | 'm' | 's';
-  children: React.ReactNode;
-}) => {
+const LIST_SPOTS = {
+  l: '•',
+  m: '◦',
+  s: '▪',
+};
+
+const List = ({ spot = 'l', children }: { spot?: 'l' | 'm' | 's'; children: React.ReactNode }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.spot}>•</div>
+      <div className={styles.spot}>{LIST_SPOTS[spot]}</div>
       <div className={styles.text}>{children}</div>
     </div>
   );
