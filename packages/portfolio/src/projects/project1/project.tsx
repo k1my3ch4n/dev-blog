1e6b4809f464b8473d75aca4b4d04b0f00bc6df2
Kim <kevin2695@naver.com>
2024-11-29 17:00:15 +0900

    feat(project) : bold , highlight 를 project 에 적용

--- packages/portfolio/src/projects/project1/project.tsx
+++ packages/portfolio/src/projects/project1/project.tsx
@@ -1,4 +1,6 @@
+import Bold from '../../components/Bold';
 import Header from '../../components/Header';
+import Highlight from '../../components/Highlight';
 import Link from '../../components/Link';
 import List from '../../components/List';
 import Text from '../../components/Text';
@@ -10,20 +12,24 @@ const project = () => {
       <Title title="🤖 관리자용 로봇 관제 웹 프로젝트" />
       <div>img</div>
       <Header size="s">기간</Header>
-      <Text>2022.04 ~ 2023.01</Text>
+      <Text>
+        <Highlight>2022.04 ~ 2023.01</Highlight>
+      </Text>
 
       <Header size="m">작업 및 성과</Header>
       <List>
-        관리자를 위한 로봇 관제 웹 프로덕트 개발
+        <Bold>관리자를 위한 로봇 관제 웹 프로덕트 개발</Bold>
         <List spot="m">로봇 제어 및 인프라 제어 웹 개발과 서비스 진행</List>
         <List spot="m">실제 동작하는 로봇의 세부 설정 , 워크스페이스의 세부 설정 기능 개발</List>
         <List spot="m">관리자 편의를 위한 UI 개발 및 제어 기능 개발</List>
       </List>
       <List>
-        프로덕트 유지보수 및 리팩토링 진행
+        <Bold>프로덕트 유지보수 및 리팩토링 진행</Bold>
         <List spot="m">
           자바스크립트 → 타입스크립트 전환
-          <List spot="s">타입스크립트 95% 전환</List>
+          <List spot="s">
+            <Bold>타입스크립트 95% 전환</Bold>
+          </List>
         </List>
         <List spot="m">
           테스트 코드 작성
@@ -38,7 +44,7 @@ const project = () => {
       <List>
         관련 홈페이지 및 자료
         <List spot="m">
-          <Link text="관련 홈페이지" link="https://twinny.ai/" />
+          <Link link="https://twinny.ai/">관련 홈페이지</Link>
         </List>
       </List>
     </>
