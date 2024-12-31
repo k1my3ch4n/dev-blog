7961447cb9e4b55f3608f017fd33f19a2d95a7f9
Kim <kevin2695@naver.com>
2024-12-31 17:47:28 +0900

    feat : 블로그 기본 페이지 초기 화면 설정

--- packages/blog/src/App.tsx
+++ packages/blog/src/App.tsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
+import styles from './App.module.scss';
+import { Divider, Header, Text, Title } from '@monorepo/core/components';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <Title title="K1MY3CH4N" />
+      <div className={styles.introduce}>
+        <div>
+          <Header size="m">질문하는 프론트엔드 개발자 입니다 .</Header>
+          <Header size="m">질문하고 경험한 것들을 기록하고 공유합니다 .</Header>
+        </div>
+        <div>
+          <Header size="m">김예찬</Header>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Title title="프로젝트" />
+      <Title title="블로그" />
+      <Title title="ETC" />
     </>
   );
-}
+};
 
 export default App;
