e44a50151ef05228c09aa5155664c8dd9797cf49
Kim <kevin2695@naver.com>
2025-01-01 18:41:03 +0900

    feat : core Header 컴포넌트 export / import 수정

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,12 +1,12 @@
 import PageBox from './PageBox';
 import Bold from './Bold';
 import Divider from './Divider';
+import Header from './Header';
 
 export { Link } from './Link';
 export { Text } from './Text';
 export { Title } from './Title';
-export { Header } from './Header';
 export { Highlight } from './Highlight';
 export { List } from './List';
 export { NumberList } from './NumberList';
-export { PageBox, Bold, Divider };
+export { PageBox, Bold, Divider, Header };
