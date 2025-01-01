143beac981ab6e5bf5178c784d448b7cd09caa94
Kim <kevin2695@naver.com>
2025-01-01 18:43:12 +0900

    feat : core Highlight 컴포넌트 export / import 수정

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -2,11 +2,11 @@ import PageBox from './PageBox';
 import Bold from './Bold';
 import Divider from './Divider';
 import Header from './Header';
+import Highlight from './Highlight';
 
 export { Link } from './Link';
 export { Text } from './Text';
 export { Title } from './Title';
-export { Highlight } from './Highlight';
 export { List } from './List';
 export { NumberList } from './NumberList';
-export { PageBox, Bold, Divider, Header };
+export { PageBox, Bold, Divider, Header, Highlight };
