d412dd414b69136c1c670ee999c458a6227341c9
Kim <kevin2695@naver.com>
2025-01-05 15:43:54 +0900

    feat : 블로그 목록 페이지 테스트 블로그 포스트 추가

--- packages/blog/src/pages/Blog/Blog.tsx
+++ packages/blog/src/pages/Blog/Blog.tsx
@@ -1,5 +1,29 @@
+import styles from './Blog.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { Divider, Header, Text, Title } from '@monorepo/core/components';
+
 const Blog = () => {
-  return <>Blog</>;
+  const navigate = useNavigate();
+
+  const handleClick = (projectName: string) => {
+    navigate(`/project/${projectName}`);
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <>
+      <Title title="📘 K1MY3CH4N's Blog" />
+      <Divider />
+
+      <div className={styles.post}>
+        <div className={styles.thumbnail}></div>
+        <div>
+          <Header size="m">테스트용 타이틀</Header>
+          <Text>테스트용 미리보기</Text>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Blog;
