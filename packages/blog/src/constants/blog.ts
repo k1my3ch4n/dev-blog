1351f9c7391e2db187de0cc77d312201f626c656
Kim <kevin2695@naver.com>
2025-01-06 17:22:01 +0900

    feat : yar workspace monorepo post 추가

--- packages/blog/src/constants/blog.ts
+++ packages/blog/src/constants/blog.ts
@@ -1,23 +1,50 @@
+// TODO : 이미지 import 수정해야함
 import { ReactComponent as Layout } from '@images/layout.svg';
 import { ReactComponent as GithubActions1 } from '@images/githubActions1.svg';
 import { ReactComponent as GithubActions2 } from '@images/githubActions2.svg';
 import { ReactComponent as GithubActions3 } from '@images/githubActions3.svg';
+import { ReactComponent as YarnWorkspaceMonorepo1 } from '@images/yarnWorkspaceMonorepo1.svg';
+import { ReactComponent as YarnWorkspaceMonorepo2 } from '@images/yarnWorkspaceMonorepo2.svg';
+import { ReactComponent as YarnWorkspaceMonorepo3 } from '@images/yarnWorkspaceMonorepo3.svg';
+import { ReactComponent as YarnWorkspaceMonorepo4 } from '@images/yarnWorkspaceMonorepo4.svg';
+import { ReactComponent as YarnWorkspaceMonorepo5 } from '@images/yarnWorkspaceMonorepo5.svg';
 
 export const BLOG_POST_DATA = [
   {
     thumbnail: '',
     title: '포스트 제목 포스트 제목 포스트 제목 포스트 제목',
     tags: ['태그 1', '태그 2'],
+    postId: '',
   },
   {
-    thumbnail: '',
-    title: '포스트 제목 포스트 제목 포스트 제목 포스트 제목',
-    tags: ['태그 1', '태그 2'],
+    thumbnail: YarnWorkspaceMonorepo5,
+    title: 'yarn workspaces 로 monorepo구성하기 #5 ( github package publish )',
+    tags: ['monorepo', 'vite', 'yarn workspace', 'yarn berry', 'github package'],
+    postId: 'yarn-workspace-monorepo-5',
   },
   {
-    thumbnail: '',
-    title: '포스트 제목 포스트 제목 포스트 제목 포스트 제목',
-    tags: ['태그 1', '태그 2'],
+    thumbnail: YarnWorkspaceMonorepo4,
+    title: 'yarn workspaces 로 monorepo구성하기 #4 ( npm publish )',
+    tags: ['monorepo', 'vite', 'yarn workspace', 'yarn berry', 'npm publish'],
+    postId: 'yarn-workspace-monorepo-4',
+  },
+  {
+    thumbnail: YarnWorkspaceMonorepo3,
+    title: 'yarn workspaces 로 monorepo구성하기 #3 ( 공통 파일 추가 )',
+    tags: ['monorepo', 'vite', 'yarn workspace', 'yarn berry'],
+    postId: 'yarn-workspace-monorepo-3',
+  },
+  {
+    thumbnail: YarnWorkspaceMonorepo2,
+    title: 'yarn workspaces 로 monorepo구성하기 #2 ( prettier , eslint 설정 )',
+    tags: ['monorepo', 'vite', 'yarn workspace', 'yarn berry', 'eslint', 'prettier'],
+    postId: 'yarn-workspace-monorepo-2',
+  },
+  {
+    thumbnail: YarnWorkspaceMonorepo1,
+    title: 'yarn workspaces 로 monorepo구성하기 #1 ( monorepo 기본 설정 )',
+    tags: ['monorepo', 'vite', 'yarn workspace', 'yarn berry'],
+    postId: 'yarn-workspace-monorepo-1',
   },
   {
     thumbnail: GithubActions3,
