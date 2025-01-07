d501bde08f8f953ab8bb8984c881b2742727c62f
Kim <kevin2695@naver.com>
2025-01-07 14:50:13 +0900

    feat : image import 수정

---
+++ packages/blog/src/assets/images/index.ts
@@ -0,0 +1,20 @@
+// todo : 삭제 예정
+export { ReactComponent as MainPhoto } from '@images/mainPhoto.svg';
+export { ReactComponent as TwinnyLogo } from '@images/twinnyLogo.svg';
+
+// blog post
+export { ReactComponent as YarnBerryPnp } from '@images/yarnBerryPnp.svg';
+
+export { ReactComponent as GithubActions1 } from '@images/githubActions1.svg';
+export { ReactComponent as GithubActions2 } from '@images/githubActions2.svg';
+export { ReactComponent as GithubActions3 } from '@images/githubActions3.svg';
+
+export { ReactComponent as YarnWorkspaceMonorepo1 } from '@images/yarnWorkspaceMonorepo1.svg';
+export { ReactComponent as YarnWorkspaceMonorepo2 } from '@images/yarnWorkspaceMonorepo2.svg';
+export { ReactComponent as YarnWorkspaceMonorepo3 } from '@images/yarnWorkspaceMonorepo3.svg';
+export { ReactComponent as YarnWorkspaceMonorepo4 } from '@images/yarnWorkspaceMonorepo4.svg';
+export { ReactComponent as YarnWorkspaceMonorepo5 } from '@images/yarnWorkspaceMonorepo5.svg';
+
+export { ReactComponent as Msw1 } from '@images/msw1.svg';
+export { ReactComponent as Msw2 } from '@images/msw2.svg';
+export { ReactComponent as Msw3 } from '@images/msw3.svg';
