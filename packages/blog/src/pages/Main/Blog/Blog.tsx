8ecf6595c285965aa95ac241f9547bbb3cb42048
Kim <kevin2695@naver.com>
2025-01-09 20:17:08 +0900

    feat : Blog 컴포넌트 생성 후 적용

---
+++ packages/blog/src/pages/Main/Blog/Blog.tsx
@@ -0,0 +1,42 @@
+import { Divider, Link, PageBox, Title } from '@monorepo/core/components';
+import styles from './Blog.module.scss';
+import { BLOG_POST_DATA } from '@src/constants/blog';
+import { useNavigate } from 'react-router-dom';
+
+const Blog = () => {
+  const navigate = useNavigate();
+
+  // todo : handleClick 수정 예정
+  const handleClick = (postId: string) => {
+    navigate(`/blog/${postId}`);
+    window.scrollTo(0, 0);
+  };
+
+  const postData = BLOG_POST_DATA.slice(0, 4);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.blogTitle}>
+        <Title title="블로그" />
+        <Link link="/blog">전체보기</Link>
+      </div>
+      <Divider />
+      <div className={styles.boxWrapper}>
+        {postData.map((data, index) => {
+          const { title, postId, Thumbnail } = data;
+
+          return (
+            <PageBox
+              key={index}
+              Thumbnail={Thumbnail}
+              title={title}
+              onClick={() => handleClick(postId)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- packages/blog/src/pages/Main/Main.tsx
+++ packages/blog/src/pages/Main/Main.tsx
@@ -1,41 +1,15 @@
-import styles from './Main.module.scss';
-import { useNavigate } from 'react-router-dom';
-
-import { Title, Divider, Link, PageBox } from '@monorepo/core/components';
-import { BLOG_POST_DATA } from '@src/constants/blog';
+import { Title } from '@monorepo/core/components';
 import Introduce from './Introduce';
 import Projects from './Projects';
+import Blog from './Blog';
 
 const Main = () => {
-  const navigate = useNavigate();
-
-  // todo : handleClick 수정 예정
-  const handleClick = (postId: string) => {
-    navigate(`/blog/${postId}`);
-    window.scrollTo(0, 0);
-  };
-
-  const postData = BLOG_POST_DATA.slice(0, 4);
-
   return (
     <>
       <Introduce />
       <Projects />
+      <Blog />
 
-      <div className={styles.blogTitle}>
-        <Title title="블로그" />
-        <Link link="/blog">전체보기</Link>
-      </div>
-      <Divider />
-      <div className={styles.boxWrapper}>
-        {postData.map((data, index) => {
-          const { title, postId, Thumbnail } = data;
-
-          return (
-            <PageBox Thumbnail={Thumbnail} title={title} onClick={() => handleClick(postId)} />
-          );
-        })}
-      </div>
       <Title title="ETC" />
     </>
   );
