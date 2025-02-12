52635a5f8dd3ebe5037792eae1351ee16b13142c
Kim <kevin2695@naver.com>
2025-02-12 23:18:31 +0900

    feat : useGetPosts 수정

--- packages/blog/src/apis/useGetPosts/useGetPosts.ts
+++ packages/blog/src/apis/useGetPosts/useGetPosts.ts
@@ -1,15 +1,28 @@
 import { useQuery } from '@apollo/client';
 import { GET_POSTS } from '@graphql/post';
+import { useEffect, useMemo, useState } from 'react';
 
 const useGetPosts = () => {
-  useQuery(GET_POSTS, {
+  const [postsData, setPostsData] = useState([]);
+
+  const { data } = useQuery(GET_POSTS, {
     onError: (error) => {
       console.log(error);
     },
-    onCompleted: (data) => {
-      console.log(data.getPosts);
-    },
   });
+
+  // todo : query 수정예정
+  const postsMemoData = useMemo(() => data?.getPosts, [data]);
+
+  useEffect(() => {
+    if (postsMemoData) {
+      setPostsData(postsMemoData);
+    }
+  }, [postsMemoData, setPostsData]);
+
+  return {
+    postsData,
+  };
 };
 
 export default useGetPosts;
