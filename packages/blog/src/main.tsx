946985082c2fe470a13a9234f1fc58a0f624642e
Kim <kevin2695@naver.com>
2025-02-12 21:14:23 +0900

    feat : graphql , client 추가

--- packages/blog/src/main.tsx
+++ packages/blog/src/main.tsx
@@ -2,9 +2,13 @@ import '@styles/global.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { ApolloProvider } from '@apollo/client';
+import client from '@clients/client.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>,
 );
