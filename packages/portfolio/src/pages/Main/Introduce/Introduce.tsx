fb3fabcaef057c46c154dabfdb4a12111de7a229
Kim <kevin2695@naver.com>
2025-02-19 01:45:57 +0900

    feat : 이미지 경로 수정

--- packages/portfolio/src/pages/Main/Introduce/Introduce.tsx
+++ packages/portfolio/src/pages/Main/Introduce/Introduce.tsx
@@ -1,5 +1,5 @@
 import styles from './Introduce.module.scss';
-import { ReactComponent as MainPhoto } from '@images/mainPhoto.svg';
+import { MainPhoto } from '@images';
 
 import { Text, Divider, Header, List, SplitGrid } from 'k1my3ch4n-core/components';
 
