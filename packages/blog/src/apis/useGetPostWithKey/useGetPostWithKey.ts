aaa18a671f18b1cc17020c85151cbde3959d9089
Kim <kevin2695@naver.com>
2025-02-18 17:30:10 +0900

    feat : useGetPostWithKey api hook 생성 및 쿼리 생성

---
+++ packages/blog/src/apis/useGetPostWithKey/useGetPostWithKey.ts
@@ -0,0 +1,36 @@
+import { useQuery } from '@apollo/client';
+import { GET_POST_WITH_KEY } from '@src/graphql/post';
+import { PostData } from '@src/recoil/postsAtom';
+import { useState } from 'react';
+
+interface PostResponseData {
+  post: PostData;
+}
+
+const adapter = (data?: PostResponseData) => {
+  if (!data) {
+    return;
+  }
+
+  return data.post;
+};
+
+const useGetPostWithKey = (postKey?: string) => {
+  const [postData, setPostData] = useState<PostData | undefined>(undefined);
+
+  const { loading: isLoading, error: isError } = useQuery<PostResponseData>(GET_POST_WITH_KEY, {
+    variables: { postKey },
+    onCompleted: (data) => setPostData(adapter(data)),
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  return {
+    isLoading,
+    isError,
+    postData,
+  };
+};
+
+export default useGetPostWithKey;
