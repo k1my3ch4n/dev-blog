96023bc9a44d484f06224445d4ba8a70447920ee
Kim <kevin2695@naver.com>
2025-02-19 13:27:42 +0900

    feat : portfolio - 블로그 페이지 추가

--- packages/portfolio/src/pages/Project/Project.tsx
+++ packages/portfolio/src/pages/Project/Project.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 
+import Blog from '@projects/blog';
 import UserRobot from '@projects/UserRobot';
 import ManagerRobot from '@projects/ManagerRobot';
 
@@ -9,6 +10,7 @@ import { ScrollToTopButton } from 'k1my3ch4n-core/components';
 const PROJECT_MAP: { [key: string]: JSX.Element } = {
   managerRobot: <ManagerRobot />,
   userRobot: <UserRobot />,
+  blog: <Blog />,
 };
 
 const Project = () => {
