a9a9ed4395d9ba490bd20fffc5ac3944348812d5
Kim <kevin2695@naver.com>
2025-02-24 00:02:19 +0900

    feat : 기본 페이지에 msw browser 추가

--- packages/blog/src/App.tsx
+++ packages/blog/src/App.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
+// ! worker 사용하고 싶을 때 주석 해제
+// import { worker } from './mocks/browser';
+
+// worker.start({ onUnhandledRequest: 'bypass' });
+
 const App = () => {
   return <RouterProvider router={router} />;
 };
