a4ddc73ce557dcf85b24aa8b1e0bfbad2ed9c8df
Kim <kevin2695@naver.com>
2025-02-26 18:03:12 +0900

    feat : Link 상수 처리

--- packages/blog/src/pages/Main/Extra/Extra.tsx
+++ packages/blog/src/pages/Main/Extra/Extra.tsx
@@ -2,6 +2,9 @@ import { Divider, PageBox, Title } from 'k1my3ch4n-core/components';
 import styles from './Extra.module.scss';
 import { Github, MainLogo } from '@images';
 
+const GITHUB_LINK = 'https://github.com/k1my3ch4n';
+const BLOG_LINK = 'https://monorepo-blog-384003056882.asia-northeast3.run.app/';
+
 const Extra = () => {
   const handleClick = (link: string) => {
     window.open(link, '_blank');
@@ -15,14 +18,14 @@ const Extra = () => {
         <PageBox
           Thumbnail={Github}
           title="Github"
-          onClick={() => handleClick('https://github.com/k1my3ch4n')}
+          onClick={() => handleClick(GITHUB_LINK)}
           width="400px"
           height="300px"
         />
         <PageBox
           Thumbnail={MainLogo}
           title="Blog"
-          onClick={() => handleClick('https://monorepo-blog-384003056882.asia-northeast3.run.app/')}
+          onClick={() => handleClick(BLOG_LINK)}
           width="400px"
           height="300px"
         />
