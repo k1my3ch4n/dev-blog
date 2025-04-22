e0ee59bf4d4a33e34665099eed9630fc88b4f5c5
Kim <kevin2695@naver.com>
2025-04-22 14:33:26 +0900

    feat : 자기소개 highlight 추가

--- packages/portfolio/src/pages/Main/Introduce/Introduce.tsx
+++ packages/portfolio/src/pages/Main/Introduce/Introduce.tsx
@@ -1,7 +1,7 @@
 import styles from './Introduce.module.scss';
 import { MainPhoto } from '@images';
 
-import { Text, Divider, Header, List, SplitGrid, Link } from 'k1my3ch4n-core/components';
+import { Text, Divider, Header, List, SplitGrid, Link, Highlight } from 'k1my3ch4n-core/components';
 
 const Introduce = () => {
   return (
@@ -44,26 +44,25 @@ const Introduce = () => {
             </List>
             <List>
               문제 해결을 주도적으로 수행하여 개발 환경을 개선하고 생산성을 향상시킨 경험이
-              있습니다. 대표적으로 CRA 환경에서 빌드 속도 지연 문제를 주도적으로 분석하고 VITE
-              마이그레이션을 통해 빌드 시간을 약 72% 단축하는 성과를 거두었습니다.
+              있습니다. 대표적으로 CRA 환경에서 빌드 속도 지연 문제를 VITE 마이그레이션을 통해
+              개선해 빌드 시간을 약 <Highlight>72%</Highlight> 단축했습니다.
+            </List>
+            <List>
+              Github Actions, AWS, Notion을 활용한{' '}
+              <Highlight>배포 및 버저닝 CI/CD 자동화</Highlight>를 구축하여 생산성을 높였습니다.
+              또한, Yarn Workspaces를 활용한 <Highlight>Monorepo 구성</Highlight> 및{' '}
+              <Highlight>Zero Install</Highlight>
+              적용을 통해 개발 환경을 최적화했습니다.
             </List>
             <List>
               다양한 직군과의 원활한 소통을 위해 노력하며 협업 효율성을 높이는 데 기여했습니다. 서버
-              팀과의 API 개발 협의를 위해 MSW를 도입하고, 디자인 팀과의 일관된 UI 개발을 위해
-              Storybook을 적극적으로 활용했습니다.
-            </List>
-            <List>
-              팀의 개발 생산성 향상을 위해 Github Actions, AWS, Notion을 활용한 배포 및 버저닝 CI/CD
-              자동화를 구축하여 운영 효율성을 높였습니다. 또한, Yarn Workspaces를 활용한 Monorepo
-              구성 및 Zero Install 적용을 통해 개발 환경을 최적화했습니다.
-            </List>
-            <List>
-              서버 개발 지연 시에는 API 명세서를 기반으로 프론트엔드 개발을 주도하고, 개발된
-              결과물을 바탕으로 서버 팀에 적극적으로 피드백을 전달하며 협업했습니다.
+              팀과의 API 개발 협의를 위해 <Highlight>MSW</Highlight>를 도입하고, 디자인 팀과의
+              일관된 UI 개발을 위해 <Highlight>Storybook</Highlight>을 적극적으로 활용했습니다.
             </List>
             <List>
               안정적인 서비스 운영을 위해 jest, React Testing Library, MSW를 활용한 테스트 코드를
-              작성하고, 테스트 커버리지를 20%에서 87%까지 향상시켜 코드 품질을 높였습니다.
+              작성하고, 테스트 커버리지를 <Highlight>20%에서 87%</Highlight>까지 향상시켜 코드
+              품질을 높였습니다.
             </List>
           </>
         }
